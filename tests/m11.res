5cd33dc3b1aabc3c4cdbe1bfee1c4afbb16bbecd
Dima Pasechnik <dima@pasechnik.info>
2024-02-09 00:45:00 +0000

    adding testing framework
    tests are invoked by
      make check
    
    Each test foo consists of
    
    1) tcenum presentation, in file named tests/foo
    2) script tests/foo.test (they all differ by name only, the contents
    are the same)
    3) results file, the tcenum's result_file for each presentation
    
    They also need to be added to Makefile.am

---
+++ tests/m11.res
@@ -0,0 +1,8 @@
+abcde..b,c,d,abcbae.
+a3b5c3d3e0c.
+a=(ce)2,(bcd)5.
+
+ INDEX =          11
+ MAX.  NO. OF COSETS DEFINED =          19
+ TOTAL NO. OF COSETS DEFINED =          19
+ 
---
+++ tests/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+resfile=$(mktemp -p/tmp tctestres.XXXXXXXXX)
+bin/tcenum tests/$gr 0 0 $resfile >/dev/null 2>&1
+
+echo 1..1
+resd=$(diff ${resfile} tests/$gr.res)
+if test "x$resd" = "x"; then 
+   echo 'ok 1'
+else
+   echo 'not ok 1 - output differs'
+   echo $resd
+fi
